034ae54948a75470d8fd3647ef2957fe312a0908
Chenhaoran <1300063762@qq.com>
2020-03-27 19:24:19 +0800

    新建项目

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/essential",
+    "@vue/standard",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "space-before-function-paren": 0,
+    indent: 0
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from './components/v-header/v-header'
+import { getSeller } from './api'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
+  },
+  components: {
+    VHeader
+  }
+}
+</script>
+
+<style lang="stylus"></style>
